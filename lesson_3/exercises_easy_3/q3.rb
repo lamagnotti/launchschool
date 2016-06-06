8a8554b16571d3384d1c5ca0e5bc1149abf1d3be
Louis Magnotti <magnottila@gmail.com>
2016-06-06 08:11:50 -0400

    Added completed exercises set 3

---
+++ lesson_3/exercises_easy_3/q3.rb
@@ -0,0 +1,4 @@
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+p flintstones.push("Dino").push("Hoppy")
+# p flintstones.concat(%w(Dino Hoppy )) #concat adds an array rather than one item
