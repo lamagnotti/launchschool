248c8076bf60182c0b994dddd419e8ec22806d05
Louis Magnotti <magnottila@gmail.com>
2016-06-09 12:00:55 -0400

    Updated excercises

--- lesson_3/exercises_medium_1/q9.rb
+++ lesson_3/exercises_medium_1/q9.rb
@@ -8,9 +8,9 @@ munsters = {
 
 munsters.each do |name, details|
   case details["age"]
-  when 0...18
+  when 0..18
     details["age_group"] = "kid"
-  when 18...65
+  when 18..65
     details["age_group"] = "adult"
   else
     details["age_group"] = "senior"
