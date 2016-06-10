14596b0029eb83abec21f4d7b94369c2d5a39957
Louis Magnotti <magnottila@gmail.com>
2016-06-10 13:58:39 -0400

    Refactor the TTT app and ran rubocop

--- lesson_4/tictactoe/tictactoe.rb
+++ lesson_4/tictactoe/tictactoe.rb
@@ -1,13 +1,18 @@
-require 'pry'
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # collumns
+                [[1, 5, 9], [3, 5, 7]]              # diagonals
 
-INITIAL_MARKER = ' '                                   # we use constants to not
-PLAYER_MARKER = 'X'                                    # have hardcoded values
-COMPUTER_MARKER = 'O'
+INITIAL_MARKER = ' '.freeze
+# we use constants to not
+PLAYER_MARKER = 'X'.freeze
+# have hardcoded values
+COMPUTER_MARKER = 'O'.freeze
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+# rubocop:disable Metrics/AbcSize
 def display_board(brd)
   system 'clear'
   puts "You're a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
@@ -25,22 +30,26 @@ def display_board(brd)
   puts "     |     |"
   puts ""
 end
+# rubocop:enable Metrics/AbcSize
 
 def initalize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] = INITIAL_MARKER}  # iterate through the range
-  new_board                                            # then populate new board
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
+  # iterate through the range
+  new_board
+  # then populate new board
 end
 
 def empty_squares(brd)
-  brd.keys.select{|num| brd[num] == INITIAL_MARKER}
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
 def player_places_piece!(brd)
   square = ''
-  loop do                                       # validation logic
+  loop do
     prompt "Choose a square! (#{empty_squares(brd).join(', ')}):"
-    square = gets.chomp.to_i                    # need to break to get out of condition
+    square = gets.chomp.to_i
+    # need to break to get out of condition
     break if empty_squares(brd).include?(square)
     prompt "Sorry, that's not a valid choice."
   end
@@ -57,34 +66,30 @@ def board_full?(brd)
 end
 
 def someone_won?(brd)
-  !!detect_winner(brd)                        # forceable turn this into a boolean
+  !!detect_winner(brd)
+  # forceable turn this into a boolean
 end
 
 def detect_winner(brd)
-  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
-                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # collumns
-                  [[1, 5, 9], [3, 5, 7]]              # diagonals
-  winning_lines.each do |line| #line is an array
-    if brd[line[0]] == PLAYER_MARKER &&
-       brd[line[1]] == PLAYER_MARKER &&
-       brd[line[2]] == PLAYER_MARKER
-       return 'Player'
-     elsif brd[line[0]] == COMPUTER_MARKER &&
-       brd[line[1]] == COMPUTER_MARKER &&
-       brd[line[2]] == COMPUTER_MARKER
+  WINNING_LINES.each do |line| # line is an array
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+      return 'Player'
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
       return 'Computer'
-     end
+    end
   end
   nil
 end
 
 loop do
-  board = initalize_board                         # board variable keeps track the state of the game
+  board = initalize_board
+  # board variable keeps track the state of the game
 
   loop do
     display_board(board)
 
-    player_places_piece!(board)                   # player chooses, see method above
+    player_places_piece!(board)
+    # player chooses, see method above
     break if someone_won?(board) || board_full?(board)
 
     computer_places_piece!(board)
