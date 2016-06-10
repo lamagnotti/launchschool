c4f774405918f95726901d02b220035952a242a6
Louis Magnotti <magnottila@gmail.com>
2016-06-10 11:52:31 -0400

    Completed TicTacToe app, now ready for use

--- lesson_4/tictactoe/tictactoe.rb
+++ lesson_4/tictactoe/tictactoe.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
-INITIAL_MARKER = ' '                        # we use constants to not
-PLAYER_MARKER = 'X'                         # have hardcoded values
+INITIAL_MARKER = ' '                                   # we use constants to not
+PLAYER_MARKER = 'X'                                    # have hardcoded values
 COMPUTER_MARKER = 'O'
 
 def prompt(msg)
@@ -10,6 +10,7 @@ end
 
 def display_board(brd)
   system 'clear'
+  puts "You're a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -37,9 +38,9 @@ end
 
 def player_places_piece!(brd)
   square = ''
-  loop do                                   # validation logic
+  loop do                                       # validation logic
     prompt "Choose a square! (#{empty_squares(brd).join(', ')}):"
-    square = gets.chomp.to_i                # need to break to get out of condition
+    square = gets.chomp.to_i                    # need to break to get out of condition
     break if empty_squares(brd).include?(square)
     prompt "Sorry, that's not a valid choice."
   end
@@ -56,17 +57,51 @@ def board_full?(brd)
 end
 
 def someone_won?(brd)
-  false
+  !!detect_winner(brd)                        # forceable turn this into a boolean
 end
 
-board = initalize_board                     # board variable keeps track the state of the game
-display_board(board)
+def detect_winner(brd)
+  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # collumns
+                  [[1, 5, 9], [3, 5, 7]]              # diagonals
+  winning_lines.each do |line| #line is an array
+    if brd[line[0]] == PLAYER_MARKER &&
+       brd[line[1]] == PLAYER_MARKER &&
+       brd[line[2]] == PLAYER_MARKER
+       return 'Player'
+     elsif brd[line[0]] == COMPUTER_MARKER &&
+       brd[line[1]] == COMPUTER_MARKER &&
+       brd[line[2]] == COMPUTER_MARKER
+      return 'Computer'
+     end
+  end
+  nil
+end
 
 loop do
-  player_places_piece!(board)                  # player chooses, see method above
-  computer_places_piece!(board)
+  board = initalize_board                         # board variable keeps track the state of the game
+
+  loop do
+    display_board(board)
+
+    player_places_piece!(board)                   # player chooses, see method above
+    break if someone_won?(board) || board_full?(board)
+
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
   display_board(board)
-  break if someone_won?(board) || board_full?(board)
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "It's a tie!"
+  end
+
+  prompt "Do you want to play again? (y or n)"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end
 
-display_board(board)
+prompt "Thanks so much for playing Tic Tac Toe! :)"
