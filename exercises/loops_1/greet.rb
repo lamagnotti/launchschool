e29b789564acc496b96aecd33a3f90c099e7b7e1
Louis Magnotti <magnottila@gmail.com>
2016-07-11 06:20:23 -0400

    Added loop exercises and solutions

---
+++ exercises/loops_1/greet.rb
@@ -0,0 +1,9 @@
+friends = ['Sarah', 'John', 'Hannah', 'Dave']
+
+# for friend in friends
+#   puts "Hello, #{friend}!"
+# end
+
+for num in 0..3
+  puts "Hello, #{friends[num]}!"
+end
---
+++ exercises/loops_1/until.rb
@@ -0,0 +1,9 @@
+numbers = [7, 9, 13, 25, 18]
+
+until numbers.empty?
+  loop do
+    puts numbers
+    break
+  end
+  break
+end
