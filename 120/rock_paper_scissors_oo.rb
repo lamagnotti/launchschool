4a3024250be3721794440f537052fd7331b9d740
Louis Magnotti <magnottila@gmail.com>
2016-09-08 05:54:47 -0400

    Refactored RPS game with clean conditional logic

--- 120/rock_paper_scissors_oo.rb
+++ 120/rock_paper_scissors_oo.rb
@@ -1,46 +1,92 @@
-class Player
-  attr_accessor :move, :name
+class Move
+  VALUES = ['rock', 'paper', 'scissors']
 
-  # Good Idea to not have a lot of logic here in the initialize
-  def initialize(player_type= :human)
-    @player_type = player_type
-    @move = nil
-    set_name
+  def initialize(value)
+    @value = value
   end
 
-  def set_name
-    if human?
-      n = ""
-      loop do
-        puts "What's your name?"
-        n = gets.chomp
-        break unless n.empty?
-        puts "Sorry, must enter a value."
-      end
-      self.name = n
-    else
-      # Setter method
-      self.name = ['R2D2', 'Hal', 'Robot123', 'Number 5'].sample
+  def scissors?
+    @value == 'scissors'
+  end
+
+  def rock?
+    @value == 'rock'
+  end
+
+  def paper?
+    @value == 'paper'
+  end
+
+  def >(other_move)
+    if rock?
+      return true if other_move.scissors?
+      return false
+    elsif paper?
+      return true if other_move.rock?
+      return false
+    elsif scissors?
+      return true if other_move.paper?
+      return false
     end
+  end
+
+  def <(other_move)
+    if rock?
+      return true if other_move.paper?
+      return false
+    elsif paper?
+      return true if other_move.scissors?
+      return false
+    elsif scissors?
+      return true if other_move.rock?
+      return false
+    end
+  end
+
+  def to_s
+    @value
+  end
+end
+
+class Player
+  attr_accessor :move, :name
+  # Good Idea to not have a lot of logic here in the initialize
+  def initialize
+    set_name
+  end
+end
+
+class Human < Player
+  def set_name
+  n = ""
+    loop do
+      puts "What's your name?"
+      n = gets.chomp.capitalize
+      break unless n.empty?
+      puts "Sorry, must enter a value."
+    end
+    self.name = n
   end
 
   def choose
-    if human?
-      choice = nil
-      loop do
-        puts "Please choose rock, paper, or scissors:"
-        choice = gets.chomp
-        break if ['rock', 'paper', 'scissors'].include? choice
-        puts "Sorry, not a valid choice. Please try again!"
-      end
-      self.move = choice
-    else
-      self.move = ['rock', 'paper', 'scissors'].sample
+    choice = nil
+    loop do
+      puts "Please choose rock, paper, or scissors:"
+      choice = gets.chomp
+      break if Move::VALUES.include? choice
+      puts "Sorry, not a valid choice. Please try again!"
     end
+    self.move = Move.new(choice)
+  end
+end
+
+class Computer < Player
+  def set_name
+    self.name = ['R2D2', 'Hal', 'Robot123', 'Number 5'].sample
   end
 
-  def human?
-    @player_type == :human
+  def choose
+    self.move = Move.new(Move::VALUES.sample)
   end
 end
 
@@ -49,12 +95,12 @@ class RPSGame
   attr_accessor :human, :computer
 
   def initialize
-    @human = Player.new
-    @computer = Player.new(:computer)
+    @human = Human.new
+    @computer = Computer.new
   end
 
   def display_welcome
-    puts "Welcome to Rock, Paper, Scissors! You Rock! :)"
+    puts "Welcome to Rock, Paper, Scissors #{human.name}! :)"
   end
 
   def display_goodbye
@@ -65,19 +111,12 @@ class RPSGame
     puts "#{human.name} chose #{human.move}."
     puts "#{computer.name} chose #{computer.move}."
 
-    case human.move
-    when 'rock'
-      puts "It's a tie!"   if computer.move == 'rock'
-      puts "#{human.name} won!"      if computer.move == 'scissors'
-      puts "#{computer.name} won!" if computer.move == 'paper'
-    when 'paper'
-      puts "It's a tie!"   if computer.move == 'paper'
-      puts "#{human.name} won!"      if computer.move == 'rock'
-      puts "#{computer.name} won!" if computer.move == 'scissors'
-    when 'scissors'
-      puts "It's a tie!"   if computer.move == 'scissors'
-      puts "#{human.name} won!"      if computer.move == 'paper'
-      puts "#{Computer} won!" if computer.move == 'rock'
+    if human.move > computer.move
+      puts "#{human.name} won!"
+    elsif human.move < computer.move
+      puts "#{computer.name} won!"
+    else
+      puts "It's a tie!"
     end
   end
 
