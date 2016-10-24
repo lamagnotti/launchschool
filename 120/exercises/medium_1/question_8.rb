c312dee0980a7f82acd2ea48a8997fb59778bdf0
Louis Magnotti <magnottila@gmail.com>
2016-10-24 14:49:19 -0400

    Completed more poker exercises

---
+++ 120/exercises/medium_1/question_8.rb
@@ -0,0 +1,30 @@
+class Card
+  attr_reader :rank, :suit
+  include Comparable
+
+  VALUES = { 'JACK' => 11, 'Queen' => 12, 'King' => 13, 'Ace' => 14 }
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def to_s
+    "#{rank} of #{suit}"
+  end
+
+  def value
+    VALUES.fetch(@rank, @rank)
+  end
+
+  def <=>(other_card)
+    value <=> other_card.value
+  end
+end
+
+cards = [Card.new(2, 'Hearts'),
+         Card.new(10, 'Diamonds'),
+         Card.new('Ace', 'Clubs')]
+#puts cards
+puts cards.min# == Card.new(2, 'Hearts')
+puts cards.max == Card.new('Ace', 'Clubs')
---
+++ 120/exercises/medium_1/question_9.rb
@@ -0,0 +1,52 @@
+class Card
+  attr_reader :rank, :suit
+  include Comparable
+
+  VALUES = { 'JACK' => 11, 'Queen' => 12, 'King' => 13, 'Ace' => 14 }
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def to_s
+    "#{rank} of #{suit}"
+  end
+
+  def value
+    VALUES.fetch(@rank, @rank)
+  end
+
+  def <=>(other_card)
+    value <=> other_card.value
+  end
+end
+
+class Deck
+  RANKS = (2..10).to_a + %w(Jack Queen King Ace).freeze
+  SUITS = %w(Hearts Clubs Diamonds Spades).freeze
+
+  def initialize
+    reset
+  end
+
+  def draw
+    reset if @deck.empty?
+    @deck.pop
+  end
+
+  private
+
+  def reset
+    @deck = RANKS.product(SUITS).map do |rank, suit|
+      Card.new(rank, suit)
+    end
+
+    @deck.shuffle!
+  end
+end
+
+deck = Deck.new
+drawn = []
+52.times { drawn << deck.draw }
+p drawn.count { |card| card.suit == 'Hearts' } == 13
