49431f44228a82d783af2806cc42d7821ed4f66f
Louis Magnotti <magnottila@gmail.com>
2016-11-16 12:12:56 -0500

    Updated views and routes

--- 170/book_viewer/book_viewer.rb
+++ 170/book_viewer/book_viewer.rb
@@ -1,16 +1,26 @@
 require "sinatra"
 require "sinatra/reloader"
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
+
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @contents = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp1.txt")
+get "/chapters/:number" do
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
+  @title = "Chapter #{number}: #{chapter_name}"
+
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
+end
+
+get "/show/:name" do
+  params[:name]
 end
